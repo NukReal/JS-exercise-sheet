93a3a0ba30963abd384db6ce9255b4f562136503
NukReal <nukrikhutsishvili94@gmail.com>
2024-03-05 19:46:53 +0400

    final change

---
+++ Exchange/scripts/Exchange.js
@@ -0,0 +1,2 @@
+var numberOfDollars = prompt("How " + "many dollars would you like to exchange?")
+alert("You will get " + (numberOfDollars * 28.79) + " hrivna");
---
+++ Questionary/questionary.js
@@ -0,0 +1,9 @@
+
+
+
+var firstName = prompt("Enter your name")
+var lastName = prompt("Enter your Last Name");
+var age = prompt("Enter your Age");
+
+console.log("User: " + firstName + " " + lastName);
+console.log("age: " + age);

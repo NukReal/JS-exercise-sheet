83c4a7786951629a67b400fae52f444fd7c4a819
Nukri Khutsishvili <158152732+NukReal@users.noreply.github.com>
2024-03-12 16:33:05 +0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,68 @@
+const getUserChoice = userInput => {
+userInput = userInput.toLowerCase();
+
+if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+  return userInput;
+} else {
+  console.log('Error!, please type: rock, paper or scissors');
+  }
+}
+
+const getComputerChoice = () => {
+  const randomNumber =
+  Math.floor(Math.random() * 3);
+ switch (randomNumber) {
+  case 0:
+    return 'rock';
+    break;
+  case 1:
+    return 'paper';
+    break;
+  case 2:
+    return 'scissors';
+    break;    
+}
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'This game is a tie!';
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'Sorry, computer won!';
+    } else {
+      return 'Congratulations, you won!';
+    }
+  }
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'Sorry, computer won!';
+    } else {
+      return 'Congratulations, you won!';
+    }
+  }
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'Sorry, computer won!';
+    } else {
+      return 'Congratulations, you won!';
+    }
+  }
+  if (userChoice === 'bomb') {
+    return 'Congratulations, you won!';
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice('rock');
+  const computerChoice = getComputerChoice();
+  console.log('You threw ' + userChoice);
+  console.log('The computer threw ' + computerChoice);
+
+  console.log(determineWinner(userChoice, computerChoice))
+};
+
+playGame()
+
+
